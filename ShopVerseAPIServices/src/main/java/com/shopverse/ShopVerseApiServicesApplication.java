6eadf3869703b545680aaea97b1662b66cd1880d
Sambit Sahoo <samkumar70773@gmail.com>
2026-07-16 19:09:25 +0530

    Your commit message

--- ShopVerseAPIServices/src/main/java/com/shopverse/ShopVerseApiServicesApplication.java
+++ ShopVerseAPIServices/src/main/java/com/shopverse/ShopVerseApiServicesApplication.java
@@ -24,7 +24,10 @@ public class ShopVerseApiServicesApplication {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                	    "http://localhost:5173",
+                	    "https://shopverseclient.onrender.com"
+                	)
                         .allowedMethods(
                                 "GET",
                                 "POST",
